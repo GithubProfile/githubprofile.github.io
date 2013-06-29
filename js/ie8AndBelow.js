97892c388bb247fcb6ba136de383d47a922c509a
Denis Petyukov <denis.petyukov@gmail.com>
2013-06-29 23:26:00 +0300

    fixed ie8 and below support

--- js/ie8AndBelow.js
+++ js/ie8AndBelow.js
@@ -4,29 +4,19 @@
             lis = document.getElementsByTagName('li'),
             lisLength = lis.length,
             html = document.getElementsByTagName('html')[0],
-            styleLisWidth = function(widthInPercents) {
-                for (var i = lisLength - 1; i >= 0; i--) {
-                    lis[i].style.width = widthInPercents + '%';
-                }
-            },
             styleFontSize = function(fontSizeInPercents) {
                 html.style.fontSize = fontSizeInPercents + '%';
             };
 
         if ( currentWidth <= 480 && currentWidth > 300) {
-            styleLisWidth(50);
             styleFontSize(90);
         } else if ( currentWidth > 480 ) {
-            styleLisWidth(33);
             styleFontSize(100);
         } else if ( currentWidth <= 300 && currentWidth > 250 ) {
-            styleLisWidth(100);
             styleFontSize(70);
         } else if ( currentWidth > 300 ) {
-            styleLisWidth(50);
             styleFontSize(90);
         } else if ( currentWidth <= 250 ) {
-            styleLisWidth(100);
             styleFontSize(50);
         }
     };

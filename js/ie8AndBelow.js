1c5467a5579b9153689b64d36d40adb939abf933
Denis Petyukov <denis.petyukov@gmail.com>
2013-06-29 23:39:42 +0300

    fixed retina ribbon media queries behavior and border on first ol

--- js/ie8AndBelow.js
+++ js/ie8AndBelow.js
@@ -1,4 +1,7 @@
 (function(){
+    var olFirstOfType = document.getElementsByTagName('ol')[0];
+        olFirstOfType.style.borderBottom = '1px solid #24404C';
+
     var detectResolutionAndChangeStyle = function(){
         var currentWidth = parseInt(document.documentElement.clientWidth),
             lis = document.getElementsByTagName('li'),
@@ -6,7 +9,8 @@
             html = document.getElementsByTagName('html')[0],
             styleFontSize = function(fontSizeInPercents) {
                 html.style.fontSize = fontSizeInPercents + '%';
-            };
+            },
+            retinaRibbon = document.getElementsByTagName('img')[0];
 
         if ( currentWidth <= 480 && currentWidth > 300) {
             styleFontSize(90);
@@ -18,6 +22,14 @@
             styleFontSize(90);
         } else if ( currentWidth <= 250 ) {
             styleFontSize(50);
+        }
+
+        if ( currentWidth <= 750 ) {
+            retinaRibbon.style.width = '74px';
+            retinaRibbon.style.height = '74px';
+        } else {
+            retinaRibbon.style.width = '149px';
+            retinaRibbon.style.height = '149px';
         }
     };
 
